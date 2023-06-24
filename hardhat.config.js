33ed878efbbb5e9702579c96cedc555775046e65
jayesh26000 <104079304+jayesh26000@users.noreply.github.com>
2023-06-25 05:29:05 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+ console.log(process.env.PRIVATE_KEY)
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
